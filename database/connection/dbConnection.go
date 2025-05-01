c92af33f82d0238c986493f69a4cd60774b07c56
RoadmannCoder <mmensheteh@gmail.com>
2025-05-01 05:56:47 +0300

    - Added Database Connection and Creating Tables With GORM instead of Go Migration

---
+++ database/connection/dbConnection.go
@@ -0,0 +1,43 @@
+package connection
+
+import (
+	"fmt"
+	"github.com/7lawa9111/Project-Based-Mentorship-Go-App/issues/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+)
+
+func NewDatabaseConnection() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSL_MODE"),
+	)
+
+	config := &gorm.Config{}
+	if os.Getenv("ENV") == "development" {
+		config.Logger = logger.Default.LogMode(logger.Info)
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	// Auto-migrate schemas
+	if err := db.AutoMigrate(&models.Author{}); err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %w", err)
+	}
+	if err := db.AutoMigrate(&models.Document{}); err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %w", err)
+	}
+	log.Println("Database connection established successfully")
+	return db, nil
+}
