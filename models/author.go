f30def7a0528df242e77f6148f10a248d15e9703
alyQamar <alyeqee@gmail.com>
2025-05-02 01:15:16 +0300

    Fix Invalid UUID format in example

--- models/author.go
+++ models/author.go
@@ -2,12 +2,13 @@ package models
 
 import (
 	_ "fmt"
-	"github.com/google/uuid"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Author struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey" example:"1"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey" example:"123e4567-e89b-12d3-a456-426614174000"`
 	FirstName string    `json:"first_name" example:"John"`
 	LastName  string    `json:"last_name" example:"Doe"`
 	CreatedAt time.Time `json:"created_at" example:"2020-09-20T14:00:00+09:00"`
