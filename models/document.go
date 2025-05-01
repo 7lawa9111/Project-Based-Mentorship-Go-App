42249048125c581478d2d155183f7c052445db46
alyQamar <alyeqee@gmail.com>
2025-05-02 02:04:36 +0300

    Add get document by id api

--- models/document.go
+++ models/document.go
@@ -8,13 +8,11 @@ import (
 )
 
 type Document struct {
-	ID       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey" example:"1"`
+	ID       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey" example:"123e4567-e89b-12d3-a456-426614174000"`
 	AuthorID uuid.UUID `gorm:"type:uuid" json:"authorId"`
 	// Author    Author    `gorm:"foreignKey:AuthorID;references:ID" json:"author"`
 	Title     string    `json:"title" example:"Document Title"`
 	Content   string    `json:"content" example:"Document Content"`
 	CreatedAt time.Time `json:"created_at" example:"2020-09-20T14:00:00+09:00"`
 	UpdatedAt time.Time `json:"updated_at" example:"2020-09-20T14:00:00+09:00"`
-	CreatedBy string    `json:"created_by" example:"John"`
-	UpdatedBy string    `json:"updated_by" example:"Doe"`
 }
