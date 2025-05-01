9b3eed3abaac2a24ca104b4f75b3bf99d4297420
mariam eissa <mariamkhaled999@gmail.com>
2025-05-01 19:42:00 +0300

    feat(add): - add the create endpoint. - add swagger for docs .

--- routes/document.go
+++ routes/document.go
@@ -1,5 +1,14 @@
 package routes
 
 import (
+	"github.com/7lawa9111/Project-Based-Mentorship-Go-App/handlers"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+func RegisterDocumentsRoutes(router *gin.Engine, db *gorm.DB) {
+	authorGroup := router.Group("/documents")
+	{
+		authorGroup.POST("", handlers.CreateDocument(db))
+	}
+}
